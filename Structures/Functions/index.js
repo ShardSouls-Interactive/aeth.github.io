001f26d89346a15bb38a3defe650891bbb8c3a14
ghost-i15 <me.isaias@icloud.com>
2025-04-16 22:26:02 -0300

    Publica o site Jekyll

--- Structures/Functions/index.js
+++
@@ -1,6 +0,0 @@
-const jsonFind = require("./JsonFind");
-const { Logger } = require("./Logger");
-const genCode = require("./GenRedeemCode");
-const PaginationEmbed = require("./PaginationEmbed");
-
-module.exports = { jsonFind, Logger, genCode, PaginationEmbed };
